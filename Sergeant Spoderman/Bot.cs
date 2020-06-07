3c1981f573d203757e3003faab0951836c1eede6
Unlucky <47713540+Unluckyx@users.noreply.github.com>
2020-06-07 19:50:03 +1000

    Add project files.

---
+++ Sergeant Spoderman/Bot.cs
@@ -0,0 +1,59 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Newtonsoft.Json;
+using Sergeant_Spoderman;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SergeantSpoderman
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += Client_Ready;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true,
+                DmHelp = true
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(DSharpPlus.EventArgs.ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Sergeant Spoderman/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SergeantSpoderman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
